e63adb9e1bca799b4c20ec9aa0e5c9a8080f9636
Ou Yanxi <c4710117@gmail.com>
2024-07-06 11:39:07 +0800

    Initial Commit

---
+++ NIL/NILApp.swift
@@ -0,0 +1,17 @@
+//
+//  NILApp.swift
+//  NIL
+//
+//  Created by T Krobot on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct NILApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
